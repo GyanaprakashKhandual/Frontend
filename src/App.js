5f651a35433ae02762bf0964fce517f92a4abc3c
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-07 13:04:37 +0530

    Alert Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react'
 import './App.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [counter, setCounter] = useState(5);
+  const [counter, setCounter] = useState(0);
+  const [showAlert, setShowAlert] = useState(false);
 
+
+    useEffect(() => {
+    let timeout;
+    if (showAlert) {
+      timeout = setTimeout(() => {
+        setShowAlert(false);
+      }, 5000); // hide after 5 seconds
+    }
+    return () => clearTimeout(timeout);
+  }, [showAlert]);
+  
   const addValue = () => {
     setCounter(counter + 1);
   }
 
   const removeValue = () => {
     setCounter( counter - 1);
+    if(counter <= 0) {
+      setCounter(0);
+      setShowAlert(true);
+      return;
+    }
+  }
+
+  if(setCounter === 0) {
+    setCounter(0);
+    alert('Can not go under the 0');
   }
   return (
     <>
     <h1>Count and Trace</h1>
     <h2>Counter Value: {counter}</h2>
-    <button onClick={addValue}>Add Value</button>
-    <button onClick={removeValue}>Remove Value</button>
+    <button onClick={addValue}>Add Value +1</button>
+    <button onClick={removeValue}>Remove Value -1</button>
+    {showAlert && (
+        <div className="custom-alert">
+          <p>Cannot go below zero!</p>
+        </div>
+      )}
     </>
   )
 }
