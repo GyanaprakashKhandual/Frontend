c0cb6bb41df1c3d7c5c417a5154bfccd33c96cdb
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-07 12:52:50 +0530

    Basic Hook Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [counter, setCounter] = useState(5);
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    setCounter( counter - 1);
+  }
   return (
-    <div>App</div>
+    <>
+    <h1>Count and Trace</h1>
+    <h2>Counter Value: {counter}</h2>
+    <button onClick={addValue}>Add Value</button>
+    <button onClick={removeValue}>Remove Value</button>
+    </>
   )
 }
 
