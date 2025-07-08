652e8a16825902a13dee9e068b777b3d2b7e0ccf
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-08 19:35:26 +0530

    Today shutdown

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { TodoProvider } from './context'
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)));
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  }
+
+  const completed = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: true } : todo
+      )
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, completed }}>
+      <div>
+
+        <h1>
+          Manage Your To Dos
+        </h1>
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
+
+      </div>
+    </TodoProvider>
+  )
 }
 
-export default App;
+export default App
