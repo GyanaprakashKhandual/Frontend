d98f5cd037c019706e80c6be48e71d1bb16f327a
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-08 18:59:23 +0530

    React Router Complete

--- reactrouter/src/index.js
+++ reactrouter/src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Layout from '../src/Layout';
 
+const router = createBrowserRouter([
+  {
+    path: "/", element: <Layout/>, children: [
+      {
+        path: '/', element: <Home/>
+      }, {
+        path: '/about', element: <About/>
+      }, {
+        path: '/contact', element: <Contact/>
+      }
+    ]
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
